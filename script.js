9accd2b6bd8a853dbc64709d5a462598a40819a5
Emad Ehsan <emadehsan@users.noreply.github.com>
2015-11-22 03:11:56 +0500

    Incomplete clock, analog clock & its resources might be removed.

--- script.js
+++ script.js
@@ -177,3 +177,37 @@ function makeDoubleDig(i) {
     }
     return i;
 }
+
+
+function analogClock() {
+ 
+    setInterval( function() {
+        var seconds = new Date().getSeconds();
+        var sdegree = seconds * 6;
+        var srotate = "rotate(" + sdegree + "deg)";
+
+        $("#sec").css({ "transform": srotate });
+
+    }, 1000 );
+      
+    setInterval( function() {
+        var hours = new Date().getHours();
+        var mins = new Date().getMinutes();
+        var hdegree = hours * 30 + (mins / 2);
+        var hrotate = "rotate(" + hdegree + "deg)";
+
+        $("#hour").css({ "transform": hrotate});
+          
+    }, 1000 );
+
+    setInterval( function() {
+        var mins = new Date().getMinutes();
+        var mdegree = mins * 6;
+        var mrotate = "rotate(" + mdegree + "deg)";
+
+        $("#min").css({ "transform" : mrotate });
+          
+    }, 1000 );
+}
+
+analogClock();
